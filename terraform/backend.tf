5fe1f89e79840fd872eb71748d7af1f8780ec3ed
jonfarias <jonathan.developer10@gmail.com>
2022-02-14 03:18:47 -0300

    terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "terraform-backend-project-id"
+    bucket = "terraform-backend-bluechat-04012022"
     prefix = "bluechat-terraform"
     credentials = "terraform-deploy.json"
   }
