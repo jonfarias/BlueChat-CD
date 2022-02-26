38c4649cefbabb939535bdd807cd2c48bc0e34fa
jonfarias <jonathan.developer10@gmail.com>
2022-02-26 16:24:07 -0300

    manifest update

--- terraform/helm_repo.sh
+++ terraform/helm_repo.sh
@@ -1,5 +1,5 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add jetstack https://charts.jetstack.io                        
-helm repo add ingress-nginx	https://kubernetes.github.io/ingress-nginx        
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx        
 helm repo add argo https://argoproj.github.io/argo-helm       
 helm repo update
