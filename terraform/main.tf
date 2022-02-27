c8981a184ff7df404bb5804186200bec23c7e095
jonfarias <jonathan.developer10@gmail.com>
2022-02-27 03:34:19 -0300

    terraform update

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,9 +66,70 @@ module "gke_auth" {
   use_private_endpoint = false
 }
 
-# ==================================================================
+# ================================================================================
 
-# ============================== HELM ==============================
+# ===================== Private Key for Linkerd (trustanchor) ====================
+
+resource "tls_private_key" "trustanchor_key" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+
+resource "tls_self_signed_cert" "trustanchor_cert" {
+  key_algorithm         = tls_private_key.trustanchor_key.algorithm
+  private_key_pem       = tls_private_key.trustanchor_key.private_key_pem
+  validity_period_hours = 87600
+  is_ca_certificate     = true
+
+  subject {
+    common_name = "identity.linkerd.cluster.local"
+  }
+
+  allowed_uses = [
+    "crl_signing",
+    "cert_signing",
+    "server_auth",
+    "client_auth"
+  ]
+}
+
+#===================================================================================
+
+# ===================== Private Key for Linkerd (issuer key) =======================
+
+resource "tls_private_key" "issuer_key" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+
+resource "tls_cert_request" "issuer_req" {
+  key_algorithm   = tls_private_key.issuer_key.algorithm
+  private_key_pem = tls_private_key.issuer_key.private_key_pem
+
+  subject {
+    common_name = "identity.linkerd.cluster.local"
+  }
+}
+
+resource "tls_locally_signed_cert" "issuer_cert" {
+  cert_request_pem      = tls_cert_request.issuer_req.cert_request_pem
+  ca_key_algorithm      = tls_private_key.trustanchor_key.algorithm
+  ca_private_key_pem    = tls_private_key.trustanchor_key.private_key_pem
+  ca_cert_pem           = tls_self_signed_cert.trustanchor_cert.cert_pem
+  validity_period_hours = 8760
+  is_ca_certificate     = true
+
+  allowed_uses = [
+    "crl_signing",
+    "cert_signing",
+    "server_auth",
+    "client_auth"
+  ]
+}
+
+# =============================================================================
+
+# ===================================== HELM ==================================
 
 resource "helm_release" "ingress-nginx" {
   name             = "ingress-nginx"
@@ -103,11 +164,32 @@ resource "helm_release" "linkerd" {
   depends_on       = [helm_release.prometheus]
   name             = "linkerd"
   repository       = "https://helm.linkerd.io/stable"
-  chart            = "linkerd2"
+  chart            = "linkerd/linkerd2"
   version          = "2.11.1"
   create_namespace = true
   namespace        = "monitoring"
+
+    set {
+    name  = "global.identityTrustAnchorsPEM"
+    value = tls_self_signed_cert.trustanchor_cert.cert_pem
+  }
+
+  set {
+    name  = "identity.issuer.crtExpiry"
+    value = tls_locally_signed_cert.issuer_cert.validity_end_time
+  }
+
+  set {
+    name  = "identity.issuer.tls.crtPEM"
+    value = tls_locally_signed_cert.issuer_cert.cert_pem
+  }
+
+  set {
+    name  = "identity.issuer.tls.keyPEM"
+    value = tls_private_key.issuer_key.private_key_pem
+  }
 }
+
 
 #resource "helm_release" "cert-manager" {
 #  name  = "cert-manager"
