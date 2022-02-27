1c1313286b126980e3a335ccf6b6f6aac46b4bb9
jonfarias <jonathan.developer10@gmail.com>
2022-02-27 03:52:52 -0300

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -160,36 +160,6 @@ resource "helm_release" "prometheus" {
   namespace        = "monitoring"
 }
 
-resource "helm_release" "linkerd" {
-  depends_on       = [helm_release.prometheus]
-  name             = "linkerd"
-  repository       = "https://helm.linkerd.io/stable"
-  chart            = "linkerd/linkerd2"
-  version          = "2.11.1"
-  create_namespace = true
-  namespace        = "monitoring"
-
-    set {
-    name  = "global.identityTrustAnchorsPEM"
-    value = tls_self_signed_cert.trustanchor_cert.cert_pem
-  }
-
-  set {
-    name  = "identity.issuer.crtExpiry"
-    value = tls_locally_signed_cert.issuer_cert.validity_end_time
-  }
-
-  set {
-    name  = "identity.issuer.tls.crtPEM"
-    value = tls_locally_signed_cert.issuer_cert.cert_pem
-  }
-
-  set {
-    name  = "identity.issuer.tls.keyPEM"
-    value = tls_private_key.issuer_key.private_key_pem
-  }
-}
-
 
 #resource "helm_release" "cert-manager" {
 #  name  = "cert-manager"
