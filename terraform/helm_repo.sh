10edf15444054993ecd340353dc73182f6b35300
jonfarias <jonathan.developer10@gmail.com>
2022-03-03 00:59:17 -0300

    circleci updates

--- terraform/helm_repo.sh
+++
@@ -1,6 +0,0 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add jetstack https://charts.jetstack.io                        
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx        
-helm repo add argo https://argoproj.github.io/argo-helm       
-helm repo add linkerd https://helm.linkerd.io/stable
-helm repo update
